0d4da6ba498a4c2f3401dc4226e2431682c733b3
Benjamin Haun <Ben@Benjamins-Mac-Pro.local>
2016-11-01 22:52:56 -0700

    Finished first page of app

---
+++ pokedex3/PokeCell.swift
@@ -0,0 +1,33 @@
+//
+//  PokeCell.swift
+//  pokedex3
+//
+//  Created by Benjamin Haun on 11/1/16.
+//  Copyright © 2016 Benjamin Haun. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder){
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(_ pokemon: Pokemon) {
+        
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+
+    
+}
---
+++ pokedex3/Pokemon.swift
@@ -0,0 +1,29 @@
+//
+//  Pokemon.swift
+//  pokedex3
+//
+//  Created by Benjamin Haun on 11/1/16.
+//  Copyright © 2016 Benjamin Haun. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    fileprivate var _name: String!
+    fileprivate var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+    
+}
